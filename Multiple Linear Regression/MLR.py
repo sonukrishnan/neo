a20ad7502e8f23a21fb85246b17bb0d0cc7c0e2a
Sonu <sonu.krishnan@gmail.com>
2017-11-05 01:51:46 +0530

    new

--- Multiple Linear Regression/MLR.py
+++ Multiple Linear Regression/MLR.py
@@ -6,17 +6,12 @@ import pandas as pd
 
 # Import Dataset
 #---------------
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values       
-Y = dataset.iloc[:,3].values
-                
-#Taking care of Missing values
-#-----------------------------
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values="NaN", strategy = "mean", axis = 0)
-imputer = imputer.fit(X[:,1:3])
-X[:,1:3] = imputer.transform(X[:,1:3])
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values   
+Y = dataset.iloc[:, 4].values
 
+# No Missing Value applicable
+                
 #Encoding of Categorical Data
 #-----------------------------
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -24,33 +19,21 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 #Dependent Variable Steps
 #1 Label Encoder for Dependent Variable X
 labelencoder_X = LabelEncoder()
-X[:,0] = labelencoder_X.fit_transform(X[:,0])
+X[:,3] = labelencoder_X.fit_transform(X[:,3])
 
 #2 OneHotEncoder for X to make dummy variables
-onehotencoder = OneHotEncoder(categorical_features= [0])
+onehotencoder = OneHotEncoder(categorical_features= [3])
 X = onehotencoder.fit_transform(X).toarray()
-#Label Encoder for Dependent Variable Y
-labelencoder_Y = LabelEncoder()
-Y = labelencoder_Y.fit_transform(Y)
 
 
+          
 #Splitting the dataset to Training and Test
 #-------------------------------------------
 from sklearn.cross_validation import train_test_split
-X_train,X_test,Y_Train,Y_Test = train_test_split(X,Y, test_size = 0.2,random_state = 0)
+X_train,X_test,Y_Train,Y_Test = train_test_split(X,Y, test_size = 1/3,random_state = 0)
 
-#Feature Scaling
-#----------------
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+# Feature Scaling not applicable as X has only one variable
 
-#Regression
-#==========
-# SLR
-#==========
-"""
 # Fitting Simple Linear Regression to the Training Set
 from sklearn.linear_model import LinearRegression
 SLR = LinearRegression()
@@ -76,12 +59,3 @@ plt.title('Salary vs Experience (Test Set)')
 plt.xlabel('Years of Exp')
 plt.ylabel('Salary')
 plt.show()
-"""
-
-#=========
-# MLR
-#=========
-
-
-
-
