78b1d05b306467967705eaa08b41a141ba164bce
Sonu <sonu.krishnan@gmail.com>
2017-11-05 20:46:04 +0530

    Updated Code

--- Polynomial Regression/PLR.py
+++ Polynomial Regression/PLR.py
@@ -13,41 +13,38 @@ Y = dataset.iloc[:, 2].values
 # No Missing Value applicable
 
 # Encoding of Categorical Data not required
-#-----------------------------
-"""
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-
-#Dependent Variable Steps
-#1 Label Encoder for Dependent Variable X
-labelencoder_X = LabelEncoder()
-X[:,0] = labelencoder_X.fit_transform(X[:,0])
-
-#2 OneHotEncoder for X to make dummy variables
-onehotencoder = OneHotEncoder(categorical_features= [0])
-X = onehotencoder.fit_transform(X).toarray()
-
-# Avoiding Dummy Variable Trap
-X = X[:, 1:]
           
-#Splitting the dataset to Training and Test
-#-------------------------------------------
-from sklearn.cross_validation import train_test_split
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size = 0.20,random_state = 0)
-"""
+#Splitting the dataset to Training and Testnot applicable as its small
+
 # Feature Scaling not applicable as X has only one variable
 
 # Fitting Simple Linear Regression to the Training Set
 from sklearn.linear_model import LinearRegression
-MLR = LinearRegression()
-MLR.fit(X,Y)
+SLR = LinearRegression()
+SLR.fit(X,Y)
 
 from sklearn.preprocessing import PolynomialFeatures
-PLR = Pol
+polynomialfeatures = PolynomialFeatures(degree=2)
+X_Poly = polynomialfeatures.fit_transform(X)
 
+PLR = LinearRegression()
+PLR.fit(X_Poly,Y)
 
+# Visualizing the Linear Regression
+plt.scatter(X, Y, color = 'red')
+plt.plot(X, SLR.predict(X), color = 'blue')
+plt.title('Simple Linear Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
-
-
+# Visualizing the Polynomial Linear Regression
+plt.scatter(X, Y, color = 'red')
+plt.plot(X, PLR.predict(polynomialfeatures.fit_transform(X)), color = 'green')
+plt.title('Polynominal Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
 
 
