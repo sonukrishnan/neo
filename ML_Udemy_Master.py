84cfaf46f97ce6f0eab650848f780e5ebd3a5a17
sonukrishnan <sonu.krishnan@gmail.com>
2017-11-07 11:32:00 +0530

    RandomForest

--- ML_Udemy_Master.py
+++ ML_Udemy_Master.py
@@ -191,3 +191,27 @@ plt.show()
 #===============
 # Random Forest
 #===============
+
+"""
+
+# Fitting Decision Tree Regression to the Training Set
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators= 100, random_state=0)
+regressor.fit(X,Y)
+
+# Predicting Value
+Y_Pred = regressor.predict(6.5)
+
+# Visualizing the DTR Regression
+# Grid is created to show minor changes and its values so that the Tree is clearly called out
+X_grid = np.arange(min(X), max (X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+
+plt.scatter(X, Y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (DTR)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+"""
+
