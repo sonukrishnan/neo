6555f813ad2d8d5fdf25c177ab474315a5e37844
Sonu <sonu.krishnan@gmail.com>
2017-11-11 21:04:08 +0530

    visualization

--- Kernel SVM/KernelSVM.py
+++ Kernel SVM/KernelSVM.py
@@ -19,31 +19,65 @@ y = dataset.iloc[:, 4].values
 # Dataset Split
 #--------------
 from sklearn.cross_validation import train_test_split
-X_Train, X_Test, y_Train, y_Test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 #Feature Scaling
 #----------------
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-X_Train = sc_X.fit_transform(X_Train)
-X_Test = sc_X.transform(X_Test)
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 
-#KNN Classification
+#Kernel SVM Classification
 #-------------
 from sklearn.svm import SVC
-classifier = SVC(random_state=0, kernel='linear')
-classifier.fit(X_Train, y_Train)
+classifier = SVC(random_state=0, kernel='rbf')
+classifier.fit(X_train, y_train)
 
 #Prediction
 #----------
-y_Pred = classifier.predict(X_Test)
+y_Pred = classifier.predict(X_test)
 
 # Confusion Matrix
 #-----------------
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_Test, y_Pred)
+cm = confusion_matrix(y_test, y_Pred)
 
 
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
  
